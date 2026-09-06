380f8a0f6990528784496ad28840f40f74e49361
Francisco Cardozo <fcardozo199@gmailcom>
2026-09-05 19:46:11 -0500

    add cors to response

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,19 @@ import routes from './adapters/routes';
 const app = express();
 app.disable('x-powered-by');
 app.use(logger('dev'));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+	res.header('Access-Control-Allow-Origin', '*'); // NOSONAR
+	res.header('Access-Control-Allow-Headers', '*'); // NOSONAR
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); // NOSONAR
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	if (req.method === 'OPTIONS') {
+		res.sendStatus(204);
+		return;
+	}
+	next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../static')));
@@ -15,13 +28,6 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 	next();
   });
 
-app.use((_, res: Response, next: NextFunction) => {
-	res.header('Access-Control-Allow-Origin', '*'); // NOSONAR
-	res.header('Access-Control-Allow-Headers', '*'); // NOSONAR
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); // NOSONAR
-	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-	next();
-});
 app.use(routes);
 
 export default app;
